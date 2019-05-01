95181d720ea3fed3d0b731b2ec9302d3558fac76
kofimorrison <49091928+kofimorrison@users.noreply.github.com>
2019-05-01 23:25:01 +0200

    Updates

---
+++ precache-manifest.a4af5f67373b56c58c99a980a911a1df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f57a4e587a1a28d88291",
+    "url": "/kontaktmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "f57a4e587a1a28d88291",
+    "url": "/kontaktmanager/static/js/main.fbe1d1c4.chunk.js"
+  },
+  {
+    "revision": "9566e09d75f1f629f8ca",
+    "url": "/kontaktmanager/static/js/runtime~main.c6462136.js"
+  },
+  {
+    "revision": "6ea41bb8d121e8bfaacb",
+    "url": "/kontaktmanager/static/css/2.ca96a16d.chunk.css"
+  },
+  {
+    "revision": "6ea41bb8d121e8bfaacb",
+    "url": "/kontaktmanager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "4d75fa00dd9176ff63c321c9708cc03e",
+    "url": "/kontaktmanager/index.html"
+  }
+];
